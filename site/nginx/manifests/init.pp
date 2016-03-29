7c0c5b34a335dc40d3963dfe72eedc4104d58605
jinx099 <raackley@gmail.com>
2016-03-29 13:06:06 -0600

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -11,10 +11,34 @@ class nginx {
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
   }
+  
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure  => file,
+    #owner   => 'root',
+    #group   => 'root',
+    #mode    => '0644',
+    source  => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx']
+    require => File['/etc/nginx/conf.d'],
+  }
+  
+  file { '/etc/nginx/conf.d':
+    ensure  =>  directory,
+  }
+  
+  file { '/var/www':
+    ensure  =>  directory,
+  }
+  
+  file { '/var/www/index.html':
+    ensure  =>  directory,
+    require =>  File['/var/www'],
+  }
 
   service { 'nginx':
     ensure    => running,
     enable    => true,
     subscribe => File['/etc/nginx/nginx.conf'],
+    subscribe => File['/etc/nginx/conf.d/default.conf'],
   }
 }
