9c56777acb64cc17f8ec77b26228d983cb840a49
jinx099 <raackley@gmail.com>
2016-03-30 07:58:46 -0600

    refactor

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,39 +1,36 @@
 class nginx {
+
+  $nginx_conf_dir = '/etc/nginx/nginx.d'
+  $doc_root = '/var/www'
+
+  File {
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
+
   package { 'nginx':
     ensure => present,
   }
 
   file { '/etc/nginx/nginx.conf':
     ensure  => file,
-    #owner   => 'root',
-    #group   => 'root',
-    #mode    => '0644',
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
   }
   
-  file { '/etc/nginx/conf.d/default.conf':
+  file { "${nginx_conf_dir}/default.conf":
     ensure  => file,
-    #owner   => 'root',
-    #group   => 'root',
-    #mode    => '0644',
     source  => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx']
   }
   
-    file { '/var/www/index.html':
+    file { "${doc_root}/index.html":
     ensure  => file,
-    #owner   => 'root',
-    #group   => 'root',
-    #mode    => '0644',
     source  => 'puppet:///modules/nginx/index.html',
   }
   
-  file { '/etc/nginx/conf.d':
-    ensure  =>  directory,
-  }
-  
-  file { '/var/www':
+  file { [$nginx_conf_dir, $doc_root]:
     ensure  =>  directory,
   }
 
