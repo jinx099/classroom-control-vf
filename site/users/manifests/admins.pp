dfdc5f1136e97021a7bc4b68e97d0a5795f6b6e4
jinx099 <raackley@gmail.com>
2016-03-30 13:46:09 -0600

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,10 +1,10 @@
 class users::admins {
   users::managed_user { 'jose': }
   users::managed_user { 'alice':
-    group => 'staff',
+    groups => 'staff',
   }
   users::managed_user { 'chen':
-    group => 'staff',
+    groups => 'staff',
     shell => '/bin/sh',
   }
 }
